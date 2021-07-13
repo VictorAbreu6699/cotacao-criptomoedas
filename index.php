0fe9ecd7b967f113fac3a3cf1c1657a5b50703c6
Victor Abreu Azevedo <azevedovictor6699@gmail.com>
2021-07-12 22:33:43 -0300

    Criação de primeiros arquivos do projeto

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+
+    include './layout/head/head.php';    
+
+?>
+
+<!doctype html>
+<html lang="pt-br">
+
+    <?php echo getHead('Cotação de Criptomoedas') ?>
+  <body>
+    <?php include './layout/header/header.php' ?>
+    
+    <?php include './pages/home.php' ?>
+
+    <?php include './layout/footer/footer.php' ?>
+  </body>
+</html>
